a355e5578bfe72d4f31e0ab57deb0f844943af91
Glenn West <glennswest@neuralcloudcomputing.com>
2017-05-14 06:30:43 +0000

    Initial Commit

---
+++ install-service.sh
@@ -0,0 +1,3 @@
+cp test1.service /etc/systemd/system/test1.service
+systemctl enable test1.service
+systemctl start test1.service
---
+++ runncc.sh
@@ -0,0 +1,4 @@
+docker kill test1.ncc9.com
+docker rm test1.ncc9.com
+docker run -d -P --name test1.ncc9.com nginxdemos/hello
+
